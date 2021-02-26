75336548da6d7a1b1d639907d4bb33362242f870
Levi Moyle <moylel1@MOYLEL1.local>
2021-02-26 11:00:56 +0800

    Hello world

--- get-quote.py
+++ get-quote.py
@@ -1,8 +1,8 @@
 def main():
-  # print("Keep it logically awesome.")
+  print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
-  #quotes = f.readlines()
+  # quotes = f.readlines()
   #f.close()
 
   #print(quotes)
